dbe68c9d5fc95b8d0dbcd3fdd5c6e0ad01d18410
unknown <subbugnaik093@gmail.com>
2023-05-27 00:58:55 +0530

    First time

---
+++ Sample.java
@@ -0,0 +1,15 @@
+iclass Sample{
+public static void main(String[]args){
+System.out.println("Hello java");
+}
+}
+class Sample{
+public static void main(String[]args){
+	int a=10;
+	int b=30;
+	if(a>b)
+	System.out.println("Hello mava");
+	else
+System.out.println("Hello java");
+}
+}
