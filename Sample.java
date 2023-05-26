c57aaee04862c14b3aced57e92ee2772bb252de0
unknown <subbugnaik093@gmail.com>
2023-05-27 01:32:30 +0530

    Second time

--- Sample.java
+++ Sample.java
@@ -7,9 +7,12 @@ class Sample{
 public static void main(String[]args){
 	int a=10;
 	int b=30;
-	if(a>b)
+	if(a>b){
 	System.out.println("Hello mava");
+	}
+	{
 	else
 System.out.println("Hello java");
+	}
 }
 }
